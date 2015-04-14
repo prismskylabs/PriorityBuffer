393bf134840a2792f0416dcb92a6b3e9af70c353
Huu Nguyen <huu@prismskylabs.com>
2015-04-14 13:06:39 -0700

    Use a hash table instead of a deque and keep the ordering logic inside the db

--- src/prioritybuffer.h
+++ src/prioritybuffer.h
@@ -3,47 +3,36 @@
 
 #include <algorithm>
 #include <chrono>
-#include <deque>
 #include <functional>
+#include <map>
 #include <sstream>
 #include <string>
-#include <tuple>
+
+#include "prioritydb.h"
 
 
 template <typename T>
 class PriorityBuffer {
     typedef std::function<unsigned long long(const T&)> PriorityFunction;
-    typedef std::tuple<unsigned long long, T, std::string> PriorityObject;
 
   public:
     PriorityBuffer(PriorityFunction make_priority=&PriorityBuffer::epoch_priority_)
-            : make_priority_{make_priority} {
+            : make_priority_{make_priority}, db_{} {
         srand(std::chrono::steady_clock::now().time_since_epoch().count());
     }
 
     void Push(const T& t) {
-        auto priority = make_priority_(t);
         auto hash = make_hash_();
-        auto find = std::find_if(objects_.begin(), objects_.end(),
-                [&priority] (const PriorityObject& o) {
-                    return priority >= get_priority_(o);
-                });
-        if (find == objects_.begin()) {
-            objects_.emplace_front(priority, t, hash);
-        } else if (find == objects_.end()) {
-            objects_.emplace_back(priority, t, hash);
-        } else {
-            auto position = find - objects_.begin();
-            std::rotate(objects_.begin(), objects_.begin() + position, objects_.end());
-            objects_.emplace_front(priority, t, hash);
-            std::rotate(objects_.begin(), objects_.end() - position, objects_.end());
-        }
+        objects_[hash] = t;
+        db_.Insert(make_priority_(t), hash);
     }
 
     T Pop() {
-        auto object = objects_.front();
-        objects_.pop_front();
-        return get_object_(object);
+        auto hash = db_.GetHighestHash();
+        auto object = objects_[hash];
+        objects_.erase(hash);
+        db_.Delete(hash);
+        return object;
     }
 
   private:
@@ -64,20 +53,9 @@ class PriorityBuffer {
         return stream.str();
     }
 
-    static std::string get_hash_(PriorityObject priority_object) {
-        return std::get<2>(priority_object);
-    }
-
-    static T get_object_(PriorityObject priority_object) {
-        return std::get<1>(priority_object);
-    }
-
-    static unsigned long long get_priority_(PriorityObject priority_object) {
-        return std::get<0>(priority_object);
-    }
-
+    PriorityDB db_;
     PriorityFunction make_priority_;
-    std::deque<PriorityObject> objects_;
+    std::map<std::string, T> objects_;
 };
 
 #endif
