8e5ebfd27aec795b9368debafcb0b24fd3b0a5c2
Huu Nguyen <huu@prismskylabs.com>
2015-04-17 14:58:15 -0700

    Add fuzzer tests

--- test/multithread_tests.cpp
+++ test/multithread_tests.cpp
@@ -20,10 +20,10 @@ unsigned long long get_priority(const PriorityMessage& message) {
     return message.priority();
 }
 
-void push(PriorityBuffer<PriorityMessage>& buffer) {
+void push(PriorityBuffer<PriorityMessage>& buffer, int messages) {
     std::random_device generator;
     std::uniform_int_distribution<unsigned long long> distribution(0, 100LL);
-    for (int i = 0; i < NUMBER_MESSAGES_IN_TEST; ++i) {
+    for (int i = 0; i < messages; ++i) {
         auto message = std::unique_ptr<PriorityMessage>{ new PriorityMessage{} };
         auto priority = distribution(generator);
         message->set_priority(priority);
@@ -43,8 +43,8 @@ void pull(PriorityBuffer<PriorityMessage>& buffer) {
     EXPECT_EQ(nullptr, buffer.Pop());
 }
 
-void pull_block(PriorityBuffer<PriorityMessage>& buffer) {
-    for (int i = 0; i < NUMBER_MESSAGES_IN_TEST; ++i) {
+void pull_block(PriorityBuffer<PriorityMessage>& buffer, int messages) {
+    for (int i = 0; i < messages; ++i) {
         auto message = buffer.Pop(true);
         EXPECT_TRUE(message->IsInitialized());
     }
@@ -54,8 +54,8 @@ void pull_block(PriorityBuffer<PriorityMessage>& buffer) {
 TEST_F(FSFixture, RandomMultithreadedTest) {
     PriorityBuffer<PriorityMessage> buffer{get_priority};
 
-    std::thread push_thread(push, std::ref(buffer));
-    std::thread pull_thread(push, std::ref(buffer));
+    std::thread push_thread(push, std::ref(buffer), NUMBER_MESSAGES_IN_TEST);
+    std::thread pull_thread(push, std::ref(buffer), NUMBER_MESSAGES_IN_TEST);
 
     push_thread.join();
     pull_thread.join();
@@ -64,11 +64,27 @@ TEST_F(FSFixture, RandomMultithreadedTest) {
 TEST_F(FSFixture, RandomMultithreadedWithBlockingTest) {
     PriorityBuffer<PriorityMessage> buffer{get_priority};
 
-    std::thread pull_thread(pull_block, std::ref(buffer));
-    std::thread push_thread(push, std::ref(buffer));
+    std::thread pull_thread(pull_block, std::ref(buffer), NUMBER_MESSAGES_IN_TEST);
+    std::thread push_thread(push, std::ref(buffer), NUMBER_MESSAGES_IN_TEST);
 
     push_thread.join();
     pull_thread.join();
+}
+
+TEST_F(FSFixture, RandomMultithreadedWithBlockingFuzzTest) {
+    PriorityBuffer<PriorityMessage> buffer{get_priority};
+    buffer.SetFuzz(1000, 1100); // This test should take ~5 seconds
+
+    auto start = std::chrono::system_clock::now();
+
+    std::thread pull_thread(pull_block, std::ref(buffer), 5);
+    std::thread push_thread(push, std::ref(buffer), 5);
+
+    push_thread.join();
+    pull_thread.join();
+
+    auto end = std::chrono::system_clock::now();
+    EXPECT_GT(end - start, std::chrono::seconds(5));
 }
 
 int main(int argc, char** argv) {
